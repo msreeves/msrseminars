008f848b513c51323da4f8eb6c98045655e9c15d
msreeves <reevesy87@hotmail.co.uk>
2023-12-21 16:07:21 +0000

    agenda panelists fix

--- templates/partials/post-listing/listing-agenda.php
+++ templates/partials/post-listing/listing-agenda.php
@@ -3,7 +3,7 @@
     <?php while( have_rows('schedule') ) : the_row(); ?>
          <div class="agenda panel h-auto"> 
             <div class="row">
-                <div class="col-lg-2">
+                <div class="col-lg-4">
     <?php while( have_rows('time') ): the_row(); 
 
         $start = get_sub_field('start');
@@ -12,12 +12,7 @@
         ?>
         <h2><?php echo $start ?> - <?php echo $finish ?></h2> 
     <?php endwhile; ?>
-
-                 </div>
-                 <div class="col-lg-6">
-                <h2><?php echo get_sub_field('name') ?> </h2> 
-                <?php echo get_sub_field('about') ?>
-                   <?php
+  <?php
     $featured_posts = get_sub_field('sponsor');
     if( $featured_posts ): ?>
     <h3> Sponsors:</h3>
@@ -35,35 +30,55 @@
                                                        $featured_post
                                                    ),
                                                ); ?>
-                                                <img src="<?php echo $image[0]; ?>" alt="alt="<?php the_post_thumbnail_caption(); ?>""> </a>   
+                                                <img src="<?php echo $image[0]; ?>" alt="<?php the_post_thumbnail_caption(); ?>"> </a>   
             <hr></hr>
     <?php endforeach; ?>
         </div>
     <?php endif; ?>
                  </div>
-                  <div class="col-lg-4">
-                    <?php
+                 <div class="col-lg-8">
+                <h2><?php echo get_sub_field('name') ?> </h2> 
+                <?php echo get_sub_field('about') ?>
+                 <?php
 $judicators = get_sub_field('judicators');
 if( $judicators ): ?>
      <h3>Judicator: </h3> 
+     <div class="panelists">
     <?php foreach( $judicators as $judicator ): 
         $permalink = get_permalink( $judicator->ID );
         $title = get_the_title( $judicator->ID );
         ?>
-            <a class="d-inline-block" href="<?php echo esc_url( $permalink ); ?>"><button><?php echo esc_html( $title ); ?></button></a>
+
+            <a href="<?php echo esc_url( $permalink ); ?>"><div class="panel"><div class="listing-image"><?php $image = wp_get_attachment_image_src(
+                                                   get_post_thumbnail_id(
+                                                       $judicator->ID,
+                                                   ),
+                                                   "single-post-thumbnail"
+                                               ); ?>
+                                                <img src="<?php echo $image[0]; ?>" alt="<?php the_post_thumbnail_caption(); ?>"></div><div class="listing-text my-auto"><h4><?php echo esc_html( $title ); ?></h4></div></div></a>
     <?php endforeach; ?>
+                                            </div>
 <?php endif; ?>
 <hr></hr>
                            <?php
 $panelists = get_sub_field('panelists');
 if( $panelists ): ?>
      <h3>Panelist: </h3> 
+      <div class="panelists">
     <?php foreach( $panelists as $panelist ): 
         $permalink = get_permalink( $panelist->ID );
         $title = get_the_title( $panelist->ID );
         ?>
-            <a class="d-inline-block" href="<?php echo esc_url( $permalink ); ?>"><button><?php echo esc_html( $title ); ?></button></a>
+
+            <a href="<?php echo esc_url( $permalink ); ?>"><div class="panel"><div class="listing-image"><?php $image = wp_get_attachment_image_src(
+                                                   get_post_thumbnail_id(
+                                                       $panelist->ID
+                                                   ),
+                                                   "single-post-thumbnail"
+                                               ); ?>
+                                                <img src="<?php echo $image[0]; ?>" alt="<?php the_post_thumbnail_caption(); ?>"></div><div class="listing-text my-auto"><h4><?php echo esc_html( $title ); ?></h4></div></div></a>
     <?php endforeach; ?>
+    </div>
 <?php endif; ?>
                  </div>
             </div>
