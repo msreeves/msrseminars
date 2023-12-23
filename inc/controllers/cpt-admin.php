80dbb8b68b05db326b7f88da1dde11e488fc84d1
msreeves <reevesy87@hotmail.co.uk>
2023-12-23 10:17:45 +0000

    version 1.0

--- inc/controllers/cpt-admin.php
+++ inc/controllers/cpt-admin.php
@@ -38,3 +38,17 @@ function ig_sort_panelist_name( $query ){
     endif;
 }
 add_action( 'pre_get_posts', 'ig_sort_panelist_name' );
+
+function ig_sort_partner_name( $query ){
+    global $pagenow;
+    
+    if( is_admin() && 'edit.php' == $pagenow && !isset( $_GET['orderby'] ) ):
+        if( $_GET['post_type'] == 'partner' && !isset( $_GET['post_status'] ) ):
+
+            $query->set( 'orderby', 'title' );
+            $query->set( 'order', 'ASC' );
+        endif;
+            
+    endif;
+}
+add_action( 'pre_get_posts', 'ig_sort_partner_name' );
--- templates/partials/post-listing/listing-partner.php
+++ templates/partials/post-listing/listing-partner.php
@@ -1,4 +1,4 @@
-         <div class="col-lg-3 mx-auto">
+         <div class="col-lg-3 col-md-6 mx-auto mb-3">
               <div class="panel">
                 <div class="partner-listing-image">
                <?php 
